4aefe124f5ae9d971704556667a9f093c5adefc2
jjavaru <jjavaru@gmail.com>
2018-02-04 10:43:17 -0600

    Lots of refactoring. (jjavaru and Marenstain worked together)

--- using_api.py
+++ using_api.py
@@ -3,33 +3,36 @@ from indeed import IndeedClient
 # set up API connection and initial query
 client = IndeedClient(publisher=4201738803816157)
 
-# query parameters
-query = "python"
-location = "53562"
-user_ip = "1.2.3.4"
-user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2)"
-start = 0
-limit = 10
-
-params = {
-    'q': query,
-    'l': location,
-    'userip': user_ip,
-    'useragent': user_agent,
-    'start': start,
-    'limit': limit
+default_params = {
+    'q': "python",
+    'l': "53562",
+    'userip': "1.2.3.4", # testing value
+    'useragent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2)" # testing value
 }
 
-job_dict = dict() # create empty dictionary to hold job results
+def find_all_jobs(params):
+    job_dict = dict() # create empty dictionary to hold job results
+    start = 0 # initialize start variable so it can be used in while loop
+    while True:
+        paged_response = search_by_page(params, start)
+        extract_job_keys(paged_response, job_dict)
+        if paged_response['totalResults'] > paged_response['end']:
+            start = paged_response['end']
+        else: break
+    return job_dict
 
-while True:
+def search_by_page(params, start=0):
+    params['start'] = start
     search_response = client.search(**params)
     print("Page #{}. Start {}. End {}. Total {}.".format(search_response['pageNumber'], search_response['start'], search_response['end'], search_response['totalResults']))
+    return search_response
+
+def extract_job_keys(search_response, job_dict):
     for result in search_response['results']:
         job_dict[result['jobkey']] = result['jobtitle']
-    if search_response['totalResults'] > search_response['end']:
-        params['start'] = search_response['end']
-    else: break
+
+job_dict = find_all_jobs(default_params)
+    
 
 for val in sorted(job_dict.values()):
-    print(val) # printing out job titles to sanity check data 
+    print(val) # print out job titles to sanity check data 
