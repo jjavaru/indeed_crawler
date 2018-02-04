8bcb5e4375cb0e681859332903928546922e04b9
MarenstainBear <mary.metzcar@gmail.com>
2018-02-03 20:09:31 -0600

    Added loop to pull down all jobs that match query.

--- using_api.py
+++ using_api.py
@@ -1,31 +1,35 @@
-# More comments. This was added after I staged this file into the index. Will it get pushed into my local repo?
-# Adding a comment just so I'll have something to add, commit, and push into github
-
 from indeed import IndeedClient
-from pprint import pprint
 
+# set up API connection and initial query
 client = IndeedClient(publisher=4201738803816157)
 
+# query parameters
+query = "python"
+location = "53562"
+user_ip = "1.2.3.4"
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2)"
+start = 0
+limit = 10
+
 params = {
-    'q': "python",
-    'l': "53562",
-    'userip': "1.2.3.4",
-    'useragent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2)"
+    'q': query,
+    'l': location,
+    'userip': user_ip,
+    'useragent': user_agent,
+    'start': start,
+    'limit': limit
 }
 
-search_response = client.search(**params)
+job_dict = dict() # create empty dictionary to hold job results
 
-job_dict = dict()
-
-for result in search_response['results']:
-    job_dict[result['jobkey']] = result['jobtitle']
+while True:
+    search_response = client.search(**params)
+    print("Page #{}. Start {}. End {}. Total {}.".format(search_response['pageNumber'], search_response['start'], search_response['end'], search_response['totalResults']))
+    for result in search_response['results']:
+        job_dict[result['jobkey']] = result['jobtitle']
+    if search_response['totalResults'] > search_response['end']:
+        params['start'] = search_response['end']
+    else: break
 
 for val in sorted(job_dict.values()):
-    print(val)
-
-# pprint(job_dict)
-# print(search_response['results'])
-print(search_response)
-
-codeanywhere = "This is surprisingly slow, and it doesn't even have a debugger!"
-pprint(codeanywhere)
+    print(val) # printing out job titles to sanity check data 
